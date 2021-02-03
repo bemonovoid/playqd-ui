bf72bff50e89fef06c2810a6d0b0ba29d9d2183f
Grygorii Kosik <bemonovoid@gmail.com>
2021-02-03 21:11:31 +0000

    Code updates

--- src/components/library/artists/ArtistsView.vue
+++ src/components/library/artists/ArtistsView.vue
@@ -2,12 +2,34 @@
 
   <div>
     <v-row>
+
       <v-col class="pl-0 text-left" md="auto">
         <v-btn depressed plain class="text-capitalize text-subtitle-1" v-bind:to="{name: 'LibraryView'}">
           <v-icon left>mdi-arrow-left</v-icon>
           <span>Library</span>
         </v-btn>
       </v-col>
+
+      <v-col class="text-right">
+        <v-menu offset-y left>
+          <template v-slot:activator="{ attrs, on}">
+            <v-btn fab small icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-horizontal</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list dense class="text-left">
+            <v-subheader>Sort artists</v-subheader>
+            <v-list-item v-for="sortType in sorting.types" @click="sortArtists(sortType.id)">
+              <v-list-item-title>{{sortType.name}}</v-list-item-title>
+              <v-list-item-icon>
+                <v-icon right>{{sortType.icon}}</v-icon>
+              </v-list-item-icon>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-col>
+
     </v-row>
     <v-row>
       <v-col class="py-0">
@@ -70,7 +92,14 @@ export default {
   data() {
     return {
       searchFilter: '',
-      artists: []
+      artists: [],
+      sorting: {
+        types: [
+          {id: 'name', name: 'By Name', icon: 'mdi-sort-alphabetical-ascending'},
+          {id: 'play-last-date', name: 'Recently Played', icon: 'mdi-sort-clock-ascending-outline'},
+          {id: 'play-count', name: 'Most Played', icon: 'mdi-sort-descending'}
+        ]
+      }
     }
   },
   methods: {
@@ -88,6 +117,28 @@ export default {
     clearInput() {
       this.searchFilter = '';
       this.filterArtists();
+    },
+    sortArtists(sortType) {
+      if (sortType === 'name') {
+        this.artists.sort((a1, a2) => {
+          let a1Name = a1.name;
+          let a2Name = a2.name;
+          return a1Name.localeCompare(a2Name);
+        });
+      }
+      if (sortType === 'play-last-date') {
+        this.artists.sort((a1, a2) => {
+          if (a1.playbackHistory.lastTimePlayed < a2.playbackHistory.lastTimePlayed) return 1;
+          if (a1.playbackHistory.lastTimePlayed > a2.playbackHistory.lastTimePlayed) return -1;
+          return 0;
+          return 0;
+        })
+      }
+      if (sortType === 'play-count') {
+        this.artists.sort((a1, a2) => {
+          return a2.playbackHistory.playCount - a1.playbackHistory.playCount;
+        })
+      }
     }
   },
   created() {
@@ -98,6 +149,7 @@ export default {
       HTTP_CLIENT.get('/library/artists/').then(response => {
         this.$store.commit('setArtists', response.data.artists);
         this.artists = this.$store.getters.getArtists;
+        this.sortArtists('play-last-date');
       }).catch(error => {
         alert(error.toString())
       });
